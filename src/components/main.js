99036917cb20c1e937744f335b28492c25b36772
GAMASTI\euclides.junior <euclides.moovefy.com.br>
2025-09-18 17:42:37 -0300

    feat: Criação de icones e estilizando-os

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import './main.css';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa';
 
 export default class main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+        'Fazer Café',
+        'Beber Água',
+        'Estudar',
+      ],
   };
 
   handleChange = (e) => {
@@ -14,7 +19,7 @@ export default class main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -30,6 +35,18 @@ export default class main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaTrash className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
